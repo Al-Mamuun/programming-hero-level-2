99ea0e8c5c7bd4afb245ebd5db839d9eb6539a16
Mamuun <almamun19403@gmail.com>
2026-08-19 13:08:07 +0600

    Generic Constraints examples

--- src/module 3/generic-constraints.ts
+++ src/module 3/generic-constraints.ts
@@ -1,4 +1,10 @@
-const addMeInMyCrushMind = <T>(myInfo:T) => {
+type CrushType = {name: string, age: number, salary: number}
+interface CrushInterface {
+    name: string;
+    age: number;
+    salary: number;
+}
+const addMeInMyCrushMind = <T extends CrushType>(myInfo:T) => {
     const crush = "Pori Moni";
     const newData = {crush,...myInfo};
     return newData;
@@ -8,13 +14,19 @@ type MyInfoType = {
     name: string;
     age: number;
     salary: number;
+    others1?: boolean;
+    others2?: null
 }
 
 const myInfo : MyInfoType = {
     name: "Mamun",
     age: 25,
-    salary: 25000
+    salary: 25000,
+    others1: false,
+    others2: null
 }
 
-const result = addMeInMyCrushMind<MyInfoType>(myInfo);
+const result = addMeInMyCrushMind(myInfo);
+const result1 = addMeInMyCrushMind({name: "Mamun", age: 24, salary: 20000});
 console.log(result);
+console.log(result1);
